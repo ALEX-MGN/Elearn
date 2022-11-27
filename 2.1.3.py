aa9c17db68c696b0b86a5003a15b576f74fc0b9f
ALEX-MGN <pereverzev0701@mail.ru>
2022-11-27 18:54:25 +0500

    1

--- 2.1.3.py
+++ 2.1.3.py
@@ -9,7 +9,7 @@ from openpyxl.styles import Border, Side, Font
 from jinja2 import Environment, FileSystemLoader
 from prettytable import PrettyTable
 
-choice = input("Inserisci Lavori / Statistiche: ")
+choice = input("Enter Vacancies/Statistics: ")
 name = input("Введите название файла: ") 
 profession = input("Введите название профессии: ")
 
