2901ee2795194b64b17fbd5fe5243d4429f7f5dc
ALEX-MGN <pereverzev0701@mail.ru>
2022-12-09 21:07:03 +0500

    3.2.1

---
+++ script.py
@@ -0,0 +1,24 @@
+dictVithVacancies = {}
+
+def main():
+    def write_chunk(part, lines):
+        with open('newCSV//data_part_'+ str(part) +'.csv', 'w', encoding="utf-8-sig") as f_out:
+            f_out.write(names)
+            f_out.writelines(lines)
+            f_out.close()
+    
+    with open('vacancies_by_year.csv', 'r', encoding="utf-8-sig") as fi_le:
+        names = fi_le.readline()
+        for string in fi_le:
+            year = string.split(",")[len(string.split(",")) - 1][0:4]
+            if(year in dictVithVacancies.keys()):
+                dictVithVacancies[year].append(string)
+            else: 
+                dictVithVacancies[year] = list(names)
+
+        if len(dictVithVacancies) > 0:
+            for vacancie in dictVithVacancies:
+                write_chunk(vacancie, dictVithVacancies[vacancie])
+
+if __name__ == '__main__':
+    main()
